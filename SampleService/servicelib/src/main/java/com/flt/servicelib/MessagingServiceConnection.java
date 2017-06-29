7fd772f25f61e976a3630a0378b188bc02e352c8
Lewis Westbury <lewis@front-line-tech.com>
2017-06-29 10:59:00 +0100

    add support for listener and bound state to the MessagingServiceConnection

--- SampleService/servicelib/src/main/java/com/flt/servicelib/MessagingServiceConnection.java
+++ SampleService/servicelib/src/main/java/com/flt/servicelib/MessagingServiceConnection.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
-import android.os.RemoteException;
 
 public class MessagingServiceConnection implements ServiceConnection {
   private boolean bound;
   private Messenger messenger;
+  private MessagingServiceConnection.Listener listener;
 
   public void send(int message, Bundle bundle) throws Exception {
     if (!bound) { throw new IllegalStateException("Not yet bound."); }
@@ -20,10 +20,23 @@ public class MessagingServiceConnection implements ServiceConnection {
     messenger.send(msg);
   }
 
+  public boolean bound() {
+    return bound;
+  }
+
+  public Listener getListener() {
+    return listener;
+  }
+
+  public void setListener(Listener listener) {
+    this.listener = listener;
+  }
+
   @Override
   public void onServiceConnected(ComponentName name, IBinder service) {
     bound = true;
     messenger = new Messenger(service);
+    listener.onConnected(this);
   }
 
   @Override
@@ -31,5 +44,11 @@ public class MessagingServiceConnection implements ServiceConnection {
     // unbind or process might have crashes
     messenger = null;
     bound = false;
+    listener.onDisconnected(this);
+  }
+
+  public interface Listener {
+    void onConnected(MessagingServiceConnection source);
+    void onDisconnected(MessagingServiceConnection source);
   }
 }
